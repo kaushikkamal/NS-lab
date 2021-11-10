e4cbc3edbb24c840e8c03da33f543169695fd6fb
kaushikkamal <kaushikborah777@gmail.com>
2021-11-10 10:32:08 +0530

    first is done

--- A1/A/client.c
+++ A1/A/client.c
@@ -11,30 +11,20 @@ int main()
   struct sockaddr_in serverAddr;
 
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-
   serverAddr.sin_family = AF_INET;
-
   serverAddr.sin_port = htons(7891);
-
   serverAddr.sin_addr.s_addr = inet_addr("10.0.0.1");
 
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
   connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
 
-  /* ----  Send something to server ---- */
-
-  strcpy(buffer, "Hi from client\n");
-
+  strcpy(buffer, "Hi\n");
   send(clientSocket, buffer, 1024, 0);
 
-  /*---- Read the message from the server into the buffer ----*/
-
   recv(clientSocket, buffer, 1024, 0);
-
-  /*---- Print the received message ----*/
-
   printf("\nData received from server: %s\n", buffer);
 
   return 0;
 }
+
